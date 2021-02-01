64b4b07edc076e1c1627a758a7376bc1beaf79c9
John Micko <jrmicko5012@gmail.com>
2021-02-01 00:04:20 -0600

    create 48 random numbers

--- src/components/PlayPage/PlayPage.js
+++ src/components/PlayPage/PlayPage.js
@@ -1,3 +1,4 @@
+import { number } from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
@@ -8,11 +9,38 @@ import './PlayPage.css'
 
 class PlayPage extends React.Component {
 
+  state = {
+    numberList: []
+  };
+
+  componentDidMount(){
+    this.makeNumbers();
+  }
+
+  makeNumbers = () => {
+    const number = [];
+    for (let i = 0; i < 48; i++) {
+      let rand = Math.floor(Math.random() * 100) + 1;
+      number.push(rand)
+      console.log(number);
+    }
+    this.setState({
+      numberList: number
+    })
+  }
+
   render() {
     return (
       <div>
         <p>Let's play BINGO!</p>
         <div className="board">
+          {
+            this.state.numberList.map((number) => {
+              return(
+                <p>{number}</p>
+              )
+            })
+          }
         </div>
       </div>
     )
