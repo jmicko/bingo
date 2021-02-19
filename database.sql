0cfda790a19c3632b65dcfebc0bf783647d3daa1
John Micko <jrmicko5012@gmail.com>
2021-02-19 01:34:49 -0600

    update db file

--- database.sql
+++ database.sql
@@ -1,10 +1,17 @@
+-- Drop existing tables to start fresh
+DROP TABLE IF EXISTS "user", "cards", CASCADE;
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "cards" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" int NOT NULL,
+  "card_numbers" int[] NOT NULL
+);
+
+ALTER TABLE "cards" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+
